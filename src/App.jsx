ff893d9af6011eb24f0b08765e3e80c40a5427aa
KoushikDutta1999 <iamkoushik1999@gmail.com>
2023-10-28 20:35:24 +0530

    feat: Show Books List in React

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 //import React from 'react'
-import { Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
-import CreateBook from "./pages/CreateBooks";
-import ShowBook from "./pages/ShowBook";
-import EditBook from "./pages/EditBook";
-import DeleteBook from "./pages/DeleteBook";
+import { Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import CreateBook from './pages/CreateBooks';
+import ShowBook from './pages/ShowBook';
+import EditBook from './pages/EditBook';
+import DeleteBook from './pages/DeleteBook';
 
 const App = () => {
   return (
@@ -15,7 +15,7 @@ const App = () => {
       <Route path='/books/edit/:id' element={<EditBook />} />
       <Route path='/books/delete/:id' element={<DeleteBook />} />
     </Routes>
-  )
-}
+  );
+};
 
-export default App
+export default App;
